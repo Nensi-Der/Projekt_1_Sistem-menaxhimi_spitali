2a0088cb85a49a8eb98b2c9b9eb1d351d6cae0b4
Nensi-Der <ndervishhasani@gmail.com>
2025-07-30 11:50:05 +0100

    Delete Medical_System (1)/Medical_System directory

--- Medical_System (1)/Medical_System/src/main/java/dev/al/constants/Roles.java
+++
@@ -1,8 +0,0 @@
-package dev.al.constants;
-
-public final class Roles {
-    public static final String DOCTOR = "DOCTOR";
-    public static final String PATIENT = "PATIENT";
-
-    private Roles() {}
-}
